494b0adcf50620e9e26f00433f0d2da008304825
Dell <vinodsharma9643@gmail.com>
2019-05-27 21:25:41 +0530

    Initial commit

---
+++ app/src/main/java/com/kashmiriguruji/cardviewandgridlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kashmiriguruji.cardviewandgridlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
