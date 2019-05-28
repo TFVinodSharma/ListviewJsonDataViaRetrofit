eb4dfef2fdaeb2e554ae764b353020d5c974608a
VINOD SHARMA <kiitvinod@gmail.com>
2019-05-28 08:26:14 +0530

    these are the files which contain json data via retrofit

---
+++ Api.java
@@ -0,0 +1,16 @@
+package com.kashmiriguruji.cardviewandgridlayout;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public interface Api {
+
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
